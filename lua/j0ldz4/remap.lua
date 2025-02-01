314b44ecd9ee11e451e569e740975385e917ce19
Joldzija <djoleepythonac@gmail.com>
2025-02-01 16:27:39 +0100

    added vim shortcuts and remap to easily access them during development

--- lua/j0ldz4/remap.lua
+++ lua/j0ldz4/remap.lua
@@ -1,2 +1,3 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set('n', '<leader>ph', ':vsplit ~/.config/nvim/vim-shortcuts.txt<CR>', { noremap = true, silent = true })

8fcb936d7d607a7cd2ace46d9424c667eec4f9f1
Joldzija <djoleepythonac@gmail.com>
2025-03-15 19:28:14 +0100

    removed unused commands

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,9 +4,7 @@ return {
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
         vim.keymap.set("n", "<leader>gd", vim.cmd.Gvdiff)
         vim.keymap.set("n", "<leader>gl", ":Git log<CR>")
-        vim.keymap.set("n", "<leader>gc", ":Git commit<CR>")
-        vim.keymap.set("n", "<leader>gpl", ":Git pull<CR>")
-        vim.keymap.set("n", "<leader>gps", ":Git push<CR>")
+        vim.keymap.set("n", "<leader>gh", ":Git help<CR>")
         vim.keymap.set("n", "<leader>gw", ":Gwrite<CR>")
         vim.keymap.set("n", "<leader>gr", ":Gread<CR>")
     end

07a10a36e73c486b013886ac8dc33347a16b329a
Joldzija <djoleepythonac@gmail.com>
2025-04-23 23:36:48 +0200

    telescope include hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,13 @@ return {
 	},
 
 	config = function()
-		require("telescope").setup({})
+		require("telescope").setup({
+			pickers = {
+				find_files = {
+					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+				},
+			},
+		})
 
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
